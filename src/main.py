c14d167d8f4c674e63ed1830875bbd6620fa9d5e
root <root@localhost>
2023-08-29 21:28:19 -0500

    auto publish 1693362499018

--- src/main.py
+++ src/main.py
@@ -1,5 +1,21 @@
-# Using Modulo
+# Modulo and Variables
 
-print(19 % 2)
 
-print(17 % 3)
+# declared Variables
+cakes = 50
+students = 24
+
+no_of_cakes_per_student = cakes / students
+cakes_remaining = cakes % students
+
+
+x = 48 
+y = 7 
+remainder = x % y 
+
+# Print Variables 
+print(remainder)
+
+print(no_of_cakes_per_student)
+
+print(cakes_remaining)
